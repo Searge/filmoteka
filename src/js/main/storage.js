44e9a12305b513e9259bb8a36931f3f409e6648d
Serhii Panasiuk <regidmail.sergej@gmail.com>
2021-12-04 19:39:44 +0200

    GUESTS-OBJECT

---
+++ src/js/main/storage.js
@@ -0,0 +1,20 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    Notify.warning(`Set state error: ${error.message}`);
+  }
+};
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    Notify.warning(`Get state error: ${error.message}`);
+  }
+};
+
+export default { save, load };
