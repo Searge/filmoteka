c5dc8aa9ebed438622a5da42df426f13d9ac757a
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-12-08 09:33:30 +0200

    правки для пагинации
    - опции пагинации вынесены в файл pagination.js
    - изменены некоторые стили

---
+++ src/js/pagination.js
@@ -0,0 +1,22 @@
+export const paginationOptions = {
+  totalItems: 20,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  centerAlign: true,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected tui-custom-styles">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
