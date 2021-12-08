0883047189a1d53c20ebac9e3c70641a530ee1ba
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-12-08 22:52:34 +0200

    Добавляет стили для пагинации
    + редактирует файо со стилями галлереи

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,18 +4,18 @@ export const paginationOptions = {
   visiblePages: 5,
   centerAlign: true,
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected tui-custom-styles">{{page}}</strong>',
+    page: '<a href="#" class="tui-page-btn-custom">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn-custom tui-is-selected-custom">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<a href="#" class="tui-page-btn-custom tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}} moveButton</span>' +
       '</a>',
     disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-page-btn-custom tui-is-disabled tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<a href="#" class="tui-page-btn-custom tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
       '</a>',
   },
