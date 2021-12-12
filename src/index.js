5c9bd7d6a9e1372de18c208755cbcb81f9d6aa2f
Searge <ashikcygan@gmail.com>
2021-12-12 09:39:55 +0200

    Bring contributors back

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/header';
 import './js/main/main-cards';
 import './js/main/movies-search';
 import './js/modal';
-//import './js/footer/contributors';
+import './js/footer/contributors';
 import './js/spinner';
 import './js/scrolling';
 import './js/mylibrary';
