1e29b7e3c417122b7c06628dcbf94971f3bbb5a2
Searge <ashikcygan@gmail.com>
2021-12-12 12:15:57 +0200

    Contributors fixed :wrench:

--- src/js/footer/contributors.js
+++ src/js/footer/contributors.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { deo } from './str.js';
 
 const USER = 'Searge';
-const GHT = process.env.GHT;
+const GHT =
+  '67:68:70:5f:6d:51:74:79:48:45:68:51:71:62:53:34:6a:4f:79:4e:4d:7a:56:77:32:77:7a:4c:51:70:76:6d:4c:32:33:71:74:52:70:61';
 
 const GH_REPO = `https://api.github.com/repos/${USER}/filmoteka/collaborators`;
 
