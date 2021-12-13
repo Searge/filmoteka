1240c372ae7c7c73d1d35e58413233c6aa2dc62a
Oleksandr <lozikanet@gmail.com>
2021-12-13 15:07:14 +0200

    myLibrary

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,6 @@
 import { myLibrary, foundFilms } from './main/guests-object';
 import { initPagination, HOME, MY_LIBRARY, site } from './pagination.js';
+import { paginationBoxEl } from './pagination.js';
 
 initPagination();
 myLibrary.initializationLibrary();
@@ -9,6 +10,7 @@ const libraryLink = document.querySelector('[id="my-library"]');
 const formHeader = document.querySelector('.header__form');
 const mylibrarybtn = document.querySelector('.header__mylibrary');
 const bgHeader = document.querySelector('.header__background');
+const cleanGallery = document.querySelector('.gallery__list');
 
 formHeader.classList.remove('is-hidden');
 homeLink.classList.add('current');
@@ -38,4 +40,6 @@ function onLoadMylibrary(e) {
   bgHeader.classList.add('library__background');
   libraryLink.classList.add('current');
   homeLink.classList.remove('current');
+  cleanGallery.innerHTML = '';
+  paginationBoxEl.classList.add('.visually-hidden');
 }
